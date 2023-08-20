cba5a7624480724a0475956f35cb57bb3077ef89
nihsok <8760998+nihsok@users.noreply.github.com>
2023-08-21 00:46:32 +0900

    refacor

--- img2grayfb.py
+++ img2grayfb.py
@@ -10,23 +10,18 @@ gray = Image.open(sys.argv[1]).convert('L')
 width_org, height_org = gray.size
 aspect_org = width_org/height_org
 if aspect_org < 1 :
-  gray = gray.rotate(90,expand=True) 
+  gray = gray.rotate(90,expand=True)
   width_org, height_org = height_org, width_org
   aspect_org = width_org/height_org
 
-# resize, 0/1, padding, and return narray 
+# resize, monochrome(0/1), padding, and return narray
 if aspect_org > width/height:#high aspect -> adjust width & pad height
   height_tmp = round(width/aspect_org)
-  img_tmp=gray.resize((width,height_tmp)).convert('1')
-  img_tmp.save('tmp.png')
   img2d=np.vstack(( 1-np.array(gray.resize((width,height_tmp)).convert('1')), np.zeros((height-height_tmp,width),dtype=np.uint8) ))
 else:#low aspect -> adjust height & pad width
   width_tmp = round(height*aspect_org)
-  img_tmp=gray.resize((width_tmp,height)).convert('1')
-  img_tmp.save('tmp.png')
   img2d=np.hstack(( 1-np.array(gray.resize((width_tmp,height)).convert('1')), np.zeros((height,width-width_tmp),dtype=np.uint8) ))
 
-print(img2d.shape)
 shift=np.uint8(range(0,8)[::-1])
 with open('tmp.buf','wb') as f:
   for j in range(0,height):
