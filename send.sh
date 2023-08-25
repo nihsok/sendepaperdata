d30ff51fde265e76b673b957dc5f2cb4641d8ea0
nihsok <8760998+nihsok@users.noreply.github.com>
2023-08-25 19:36:00 +0900

    edit path in send.sh

--- send.sh
+++ send.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python3 img2grayfb.py ~/fax/tmp.png
+python3 ~/sendepaperdata/img2grayfb.py tmp.png
 timeout 2 curl -X POST  -H "Content-Type: application/octet-stream" --data-binary @tmp.buf http://192.168.10.108
