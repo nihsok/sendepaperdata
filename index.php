dcebacc7b14b76ae126e1b5fb364585b453f46c5
nihsok <8760998+nihsok@users.noreply.github.com>
2023-08-26 21:10:23 +0900

    upload setting

--- index.php
+++ index.php
@@ -2,20 +2,34 @@
 <html>
 <head>
   <meta charset='utf-8'>
-  <title>upload & send</title>
+  <title>upload & compile & send</title>
 </head>
 <body>
-<form enctype="multipart/form-data" method="POST">
+
+<form enctype="multipart/form-data" method="POST" action="">
   <input type="file" name="img" accept="image/*"/>
   <input type="submit" value="upload"/>
 </form>
+
 <?php
+switch($_FILES['img']['error']){
+  case UPLOAD_ERR_OK:
+    break;
+  case UPLOAD_ERR_NO_FILE:
+    echo 'no file';
+    break;
+  default:
+    echo 'File upload error:'.$_FILES['img']['error'];
+}
 $tmp_file = $_FILES['img']['tmp_name'];
 if(move_uploaded_file($tmp_file,'./tmp.png')){
-  echo "Uploaded completed.";
+  echo "Uploaded.";
+  echo '<img src="tmp.png">';
+  exec('bash ./send.sh',$return);
+}else{
+  echo "file preparation error.";
 }
-exec('bash send.sh',$return);
-echo $return;
 ?>
+
 </body>
 </html>
--- send.sh
+++ send.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-python3 ~/sendepaperdata/img2grayfb.py tmp.png
+python3 ./img2grayfb.py tmp.png
 timeout 2 curl -X POST  -H "Content-Type: application/octet-stream" --data-binary @tmp.buf http://192.168.10.108
+rm tmp.buf
