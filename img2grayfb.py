a4dc6bf022963504054fda95ad789536511421cd
nihsok <8760998+nihsok@users.noreply.github.com>
2023-10-29 21:03:09 +0900

    L->LA,wb&rwb flag

--- img2grayfb.py
+++ img2grayfb.py
@@ -2,11 +2,12 @@ from PIL import Image
 import numpy as np
 import sys
 
+inv_flag = True #True fo wb, False for rwb
 width = 800
 height = 480
 rotate = 0
 
-gray = Image.open(sys.argv[1]).convert('L')
+gray = Image.open(sys.argv[1]).convert('LA')
 width_org, height_org = gray.size
 aspect_org = width_org/height_org
 if aspect_org < 1 :
@@ -15,13 +16,14 @@ if aspect_org < 1 :
   aspect_org = width_org/height_org
 
 # resize, monochrome(0/1), padding, and return narray
-if aspect_org > width/height:#high aspect -> adjust width & pad height
+if aspect_org > width/height: #high aspect -> adjust width & pad height
   height_tmp = round(width/aspect_org)
-  img2d=np.vstack(( np.array(gray.resize((width,height_tmp)).convert('1')), np.ones((height-height_tmp,width),dtype=np.uint8) ))
-else:#low aspect -> adjust height & pad width
+  img2d = np.vstack(( np.array(gray.resize((width,height_tmp)).convert('1')), np.ones((height-height_tmp,width),dtype=np.uint8) ))
+else: #low aspect -> adjust height & pad width
   width_tmp = round(height*aspect_org)
-  img2d=np.hstack(( np.array(gray.resize((width_tmp,height)).convert('1')), np.ones((height,width-width_tmp),dtype=np.uint8) ))
+  img2d = np.hstack(( np.array(gray.resize((width_tmp,height)).convert('1')), np.ones((height,width-width_tmp),dtype=np.uint8) ))
 
+if inv_flag: img2d = 1-img2d
 shift=np.uint8(range(0,8)[::-1])
 with open('tmp.buf','wb') as f:
   for j in range(0,height):
--- send.sh
+++ send.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 python3 ./img2grayfb.py tmp.png
-timeout 2 curl -X POST  -H "Content-Type: application/octet-stream" --data-binary @tmp.buf http://192.168.10.108
+timeout 3 curl -X POST  -H "Content-Type: application/octet-stream" --data-binary @tmp.buf http://192.168.10.108
 rm tmp.buf
